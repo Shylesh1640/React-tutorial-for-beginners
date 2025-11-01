77e5089d2534a183ca0fe278ee884c8912159558
unknown <shylesh1640@gmail.com>
2025-11-01 23:40:57 +0530

    probs in react

---
+++ properties/src/App.jsx
@@ -0,0 +1,17 @@
+import Student from "./Student"
+
+function App() {
+  
+
+  return (
+    <>
+      <Student name="Alice" age={20} grade="A" />
+      <Student name="Bob" age={22} grade="B" />
+      <Student name="Charlie" age={19} grade="A+" />
+      <Student />
+      
+    </>
+  )
+}
+
+export default App
---
+++ properties/src/Student.jsx
@@ -0,0 +1,25 @@
+function Student(props) {
+
+    return(
+        <div className="student">
+            <h2>Student</h2>
+            <p>Name: {props.name}</p>
+            <p>Age: {props.age}</p>
+            <p>Grade: {props.grade}</p>
+        </div>
+
+    );
+
+}
+// Student.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     age: PropTypes.number.isRequired,
+//     grade: PropTypes.string.isRequired
+// }; 
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    grade: "N/A"
+}
+export default Student
