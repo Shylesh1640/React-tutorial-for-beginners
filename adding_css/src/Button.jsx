51231db1e127b6dc26b49c34d3c1a5f242193060
unknown <shylesh1640@gmail.com>
2025-11-01 10:38:10 +0530

    adding css

---
+++ adding_css/src/Button.jsx
@@ -0,0 +1,18 @@
+// import styles from './Button.module.css'
+function Button() {
+
+    const styles ={
+            backgroundColor: "hsl(200, 100%, 50%)",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+    }
+
+    return(
+        <button style={styles}>Click Me</button>
+
+    );
+} 
+export default Button
