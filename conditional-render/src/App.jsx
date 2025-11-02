be071efa509cfa1c2e3e836968f2d1148b16d6c8
unknown <shylesh1640@gmail.com>
2025-11-02 12:05:25 +0530

    conditional rendering in react

---
+++ conditional-render/src/App.jsx
@@ -0,0 +1,14 @@
+import Usergreet from "./Usergreet"
+
+function App() {
+  
+
+  return (
+    <>
+    <Usergreet isLoggedIn={true} username ="fugitive"/>
+         
+    </>
+  )
+}
+
+export default App
---
+++ conditional-render/src/Usergreet.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types"
+function Usergreet(props){
+
+    // if(props.isLoggedIn){
+    //     return(<h2 className="logedin">Welcome {props.username}</h2>)
+    // }
+    // else{
+    //     return(<h2 className="login">Please log in</h2>)
+    // }
+
+    const welcomemsg = <h2 className="logedin">Welcome {props.username}</h2>
+    const loginmsg = <h2 className="login">Please log in</h2>
+
+    return(
+        props.isLoggedIn ? welcomemsg : loginmsg  );
+    
+    
+}
+Usergreet.propTypes = {
+    isLoggedIn: PropTypes.bool.isRequired,
+    username: PropTypes.string
+}
+Usergreet.defaultProps = {
+    isloggedIn: false,
+    username: "Guest"
+}
+export default Usergreet
